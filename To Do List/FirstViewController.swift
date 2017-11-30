f8d372958aba3bdda30c1673c05150bef9167abb
Charlie <charliecuddy4@gmail.com>
2017-11-29 23:28:56 -0600

    delete an item from the table view and update the table

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableOfItems: UITableView!
+    
+    var items:[String] = []
 
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let updatedListObject = UserDefaults.standard.object(forKey: "items")
+        
+        if let tempItems = updatedListObject as? [String] {
+            
+            items = tempItems
+        }
+        
+        tableOfItems.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            items.remove(at: indexPath.row)
+            
+            tableOfItems.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var newItem: UITextField!
+    
+    @IBAction func addItem(_ sender: Any) {
+        
+        let updatedListObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = updatedListObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(newItem.text!)
+            
+        } else {
 
+            items = [newItem.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        newItem.text = ""
+    }
+    
+    // get rid of keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //bring keyboard back
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
